fd88789811afe2925936536e825881cc2b9ead7f
2dmitrypavlov <2dmitrypavlov@gmail.com>
2015-01-12 19:18:09 +0200

    ready model

---
+++ src/test/java/um/re/test/tester.scala
@@ -0,0 +1,24 @@
+package um.re.test
+
+
+import scala.io.Codec.string2codec
+import um.re.es.emr.NumberFinder
+
+object tester {
+	def main(args:Array[String]){
+		val nf = new NumberFinder
+		val jsonStr = scala.io.Source.fromFile("C:\\test\\jsonArray.txt")("ISO_8859-1").getLines.toList.filter{l=>
+		  l.startsWith("""			"url": """)||
+		  l.startsWith("""			"price_prop1": """)||
+		  l.startsWith("""			"price_updated": """)}
+		.map{l=> 
+		  val trimFrom = l.indexOf("""": """")
+		  l.substring(trimFrom+4, l.length()-1)}.toArray
+		for(i <- 0 to 49){
+		  var id = i*3
+		  println(nf.find(jsonStr(id), jsonStr(id+1)))
+		  
+		}
+		
+	}
+}

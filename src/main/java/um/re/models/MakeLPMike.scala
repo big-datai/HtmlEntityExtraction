10725949a64348791c489aabb7654206e624b5ce
mkras <michaelkras@gmail.com>
2015-02-28 20:13:56 +0200

    added calculations of TP TN FP FN

--- src/main/java/um/re/models/MakeLPMike.scala
+++ src/main/java/um/re/models/MakeLPMike.scala
@@ -61,34 +61,69 @@ object MakeLPMike extends App {
   val splits = parsedData.randomSplit(Array(0.7, 0.3))
   val (trainingData, test) = (splits(0), splits(1))
 
-  //trainng tfidf
+  //trainng idf
   val hashingTF = new HashingTF(50000)
   val tf: RDD[Vector] = hashingTF.transform(trainingData.map(l => l._2))
 
   val idf = (new IDF(minDocFreq = 10)).fit(tf)
   val idf_vector  = idf.idf.toArray
-  val tfidf: RDD[Vector] = idf.transform(tf)
+
+  def tf_to_tfidf(rdd:RDD[Vector]):RDD[Vector] ={
+    val idf_vals = idf_vector
+    rdd.map{tf_vals => 
+      		val tfidfArr = (tf_vals.toArray,idf_vals).zipped.map((d1,d2)=>d1*d2)
+      		Vectors.dense(tfidfArr)}
+  }
+  
+  //training tfidf
+  val tf_train_p: RDD[Vector] = hashingTF.transform(trainingData.filter(t=> t._1==1).map(l => l._2))
+  val tf_train_n: RDD[Vector] = hashingTF.transform(trainingData.filter(t=> t._1==0).map(l => l._2))
+  
+  val tfidf_train_p = tf_to_tfidf(tf_train_p)
+  val tfidf_train_n = tf_to_tfidf(tf_train_n)
+  
+  val positive_train = tfidf_train_p.map { l => LabeledPoint(1, l) }
+  val negat_train = tfidf_train_n.map { l => LabeledPoint(0, l) }
+  //ALL TOGETHER
+  val points_train = positive_train ++ negat_train
+  
   
   //test tfidf
   
   val tf_test_p: RDD[Vector] = hashingTF.transform(test.filter(t=> t._1==1).map(l => l._2))
   val tf_test_n: RDD[Vector] = hashingTF.transform(test.filter(t=> t._1==0).map(l => l._2))
   
-  def tf_to_tfidf(rdd:RDD[Vector]):RDD[Vector] ={
-    val idf_vals = idf_vector
-    rdd.map{tf_vals => 
-      		val tfidfArr = (tf_vals.toArray,idf_vals).zipped.map((d1,d2)=>d1*d2)
-      		Vectors.dense(tfidfArr)}
-  } 
+ 
   val tfidf_test_p = tf_to_tfidf(tf_test_p)
   val tfidf_test_n = tf_to_tfidf(tf_test_n)
   
   val positive = tfidf_test_p.map { l => LabeledPoint(1, l) }
   val negat = tfidf_test_n.map { l => LabeledPoint(0, l) }
   //ALL TOGETHER
-  val points = positive ++ negat
+  val points_test = positive ++ negat
+  
+  // train model
+  val boostingStrategy =
+    BoostingStrategy.defaultParams("Classification")
+  boostingStrategy.numIterations = 3 // Note: Use more in practice
+  val model =
+    GradientBoostedTrees.train(points_train, boostingStrategy)
+
+  // Evaluate model on test instances and compute test error
+  val labelAndPreds = points_test.map { point =>
+    val prediction = model.predict(point.features)
+    (point.label, prediction)
+  }
+
+  val tp = labelAndPreds.filter{case (l,p) => (l==1)&&(p==1) }.count
+  val tn = labelAndPreds.filter{case (l,p) => (l==0)&&(p==0) }.count
+  val fn = labelAndPreds.filter{case (l,p) => (l==0)&&(p==1) }.count
+  val fp = labelAndPreds.filter{case (l,p) => (l==1)&&(p==0) }.count
+  
+  
+  
   //SAVE TO FILE ALL DATA
-  MLUtils.saveAsLibSVMFile(points, "hdfs:///pavlovout/points")
+  /*MLUtils.saveAsLibSVMFile(points, "hdfs:///pavlovout/points")
   MLUtils.saveAsLibSVMFile(points, "s3://pavlovout/points")
   // val tf_ppoints = tf.map { l => LabeledPoint(1, l) } ++ tfn.map { l => LabeledPoint(0, l) }
   //SAVE TO FILE ALL DATA
@@ -97,6 +132,6 @@ object MakeLPMike extends App {
   test.saveAsObjectFile("hdfs:///pavlovout/test_obj")
   val l = sc.textFile("hdfs:///pavlovout/test_text")
   val ll = sc.objectFile("hdfs:///pavlovout/test_obj")
-
+*/
   
 }

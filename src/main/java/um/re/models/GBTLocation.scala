a8aece51ddbf7ca149804489c07a5b7c9c1a2b5c
mkras <michaelkras@gmail.com>
2015-03-01 21:02:04 +0200

    created GBT model with location

--- src/main/java/um/re/models/GBTLocation.scala
+++ src/main/java/um/re/models/GBTLocation.scala
@@ -32,7 +32,7 @@ import org.apache.spark.mllib.linalg.SparseVector
 import org.apache.spark.mllib.feature.IDF
 
 
-object MakeLPMike extends App {
+object GBTLocation extends App {
 
   val conf_s = new SparkConf().setAppName("es").set("master", "yarn-client").set("spark.serializer", classOf[KryoSerializer].getName)
   val sc = new SparkContext(conf_s)
@@ -109,78 +109,5 @@ object MakeLPMike extends App {
   val fp = labelAndPreds.filter{case (l,p) => (l==0)&&(p==1) }.count
   val fn = labelAndPreds.filter{case (l,p) => (l==1)&&(p==0) }.count
 
-  
-  
-  /*def tf_to_tfidf(rdd:RDD[Vector]):RDD[Vector] ={
-    val idf_vals = idf_vector
-    rdd.map{tf_vals => 
-      		val tfidfArr = (tf_vals.toArray,idf_vals).zipped.map((d1,d2)=>d1*d2)
-      		val values = tfidfArr.filter{l=>l!=0}
-      		val index = tfidfArr.zipWithIndex.filter{l=> l._1!=0 }.map{l=>l._2}
-      		Vectors.sparse(50000,index, values)}
-  }
-  
-  //training tfidf
-  val tf_train_p: RDD[Vector] = hashingTF.transform(trainingData.filter(t=> t._1==1).map(l => l._2))
-  val tf_train_n: RDD[Vector] = hashingTF.transform(trainingData.filter(t=> t._1==0).map(l => l._2))
-  
-  val tfidf_train_p = tf_to_tfidf(tf_train_p)
-  val tfidf_train_n = tf_to_tfidf(tf_train_n)
-  
-  val positive_train = tfidf_train_p.map { l => LabeledPoint(1, l) }
-  val negat_train = tfidf_train_n.map { l => LabeledPoint(0, l) }
-  //ALL TOGETHER
-  val points_train = positive_train ++ negat_train
-  
-  points_train.partitions.size
-  //test tfidf
-  
-  val tf_test_p: RDD[Vector] = hashingTF.transform(test.filter(t=> t._1==1).map(l => l._2))
-  val tf_test_n: RDD[Vector] = hashingTF.transform(test.filter(t=> t._1==0).map(l => l._2))
-  
- 
-  val tfidf_test_p = tf_to_tfidf(tf_test_p)
-  val tfidf_test_n = tf_to_tfidf(tf_test_n)
-  
-  val positive = tfidf_test_p.map { l => LabeledPoint(1, l) }
-  val negat = tfidf_test_n.map { l => LabeledPoint(0, l) }
-  //ALL TOGETHER
-  val points_test = positive ++ negat
-  //points_train.repartition(192)
-  // train model
-  val boostingStrategy =
-    BoostingStrategy.defaultParams("Classification")
-  boostingStrategy.numIterations = 40 // Note: Use more in practice
-  boostingStrategy.treeStrategy.maxDepth = 2
-  val model =
-    GradientBoostedTrees.train(points_train, boostingStrategy)
-
-  // Evaluate model on test instances and compute test error
- def labelAndPred(input_points:RDD[LabeledPoint])={
-    val local_model = model
-	  val labelAndPreds = input_points.map { point =>
-	  val prediction = local_model.predict(point.features)
-	  (point.label, prediction)
-	  }
-   labelAndPreds
-    
-  }
-  val labelAndPreds = labelAndPred(points_test)
-  val tp = labelAndPreds.filter{case (l,p) => (l==1)&&(p==1) }.count
-  val tn = labelAndPreds.filter{case (l,p) => (l==0)&&(p==0) }.count
-  val fp = labelAndPreds.filter{case (l,p) => (l==0)&&(p==1) }.count
-  val fn = labelAndPreds.filter{case (l,p) => (l==1)&&(p==0) }.count
-  */
-  //SAVE TO FILE ALL DATA
-  /*MLUtils.saveAsLibSVMFile(points, "hdfs:///pavlovout/points")
-  MLUtils.saveAsLibSVMFile(points, "s3://pavlovout/points")
-  // val tf_ppoints = tf.map { l => LabeledPoint(1, l) } ++ tfn.map { l => LabeledPoint(0, l) }
-  //SAVE TO FILE ALL DATA
-  MLUtils.saveAsLibSVMFile(points, "hdfs:///pavlovout/points")
-  test.saveAsTextFile("hdfs:///pavlovout/test_text")
-  test.saveAsObjectFile("hdfs:///pavlovout/test_obj")
-  val l = sc.textFile("hdfs:///pavlovout/test_text")
-  val ll = sc.objectFile("hdfs:///pavlovout/test_obj")
-*/
   
 }

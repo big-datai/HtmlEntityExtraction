f33d588a5322d2ac46c07d7d1cb0a3ba062c9398
“2dmitry.pavlov” <2dmitrypavlov@gmail.com>
2015-03-21 20:42:05 +0200

    update submit-per-domain.sh

--- submit-per-domain.sh
+++ submit-per-domain.sh
@@ -1,3 +1,9 @@
-or d in $(cat ~/domains.list); do
+i = 0
+for d in $(cat ~/domains.list); do
+i=$((i+1))
 nohup ~/spark/bin/spark-submit --jars $(echo /home/hadoop/*.jar | tr ' ' ',') --class "um.re.models.GBTPerDomain" --master yarn-cluster /home/hadoop/sparkmain/target/sparkmain-1.0-SNAPSHOT.jar $d &
+if (($i == 10)); then
+sleep 20
+i=0
+fi
 done

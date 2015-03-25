fc909d29bfcbe5640964aa1a256c3c43382de762
2dmitrypavlov <2dmitrypavlov@gmail.com>
2015-03-25 20:18:32 +0200

    BUILSCANDID

--- src/main/java/um/re/utils/EsUtils.scala
+++ src/main/java/um/re/utils/EsUtils.scala
@@ -22,8 +22,8 @@ import org.apache.hadoop.io.NullWritable
 import org.elasticsearch.hadoop.mr.EsOutputFormat
 
 object EsUtils {
-  val ESIP = "54.159.1.156"
-  val ESINDEX = "candidl/data"
+  val ESIP = "54.152.94.149"
+  val ESINDEX = "candidl5/data"
   val conf = new JobConf()
   conf.set("es.resource", "process_count/counter")
   conf.set("es.query", "?q=updatePriceCount")

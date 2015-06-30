db7825d0f0e69259fa5f4f3b6ccb4400e3c5d1ba
2dmitrypavlov <2dmitrypavlov@gmail.com>
2015-06-30 14:16:32 +0300

    moved to spark 1.3.1 , finalized S3ToKafka

---
+++ src/main/java/um/re/emr/S3ToKafka.scala
@@ -0,0 +1,57 @@
+package um.re.emr
+
+import org.apache.spark.SparkContext
+import org.apache.spark._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka.KafkaUtils
+import kafka.serializer.StringDecoder
+import kafka.producer._
+import org.apache.spark.streaming.Seconds
+import java.util.Properties
+import um.re.utils.Utils
+import com.utils.messages.MEnrichMessage
+import kafka.serializer.DefaultEncoder
+
+object S3ToKafka { //}extends App {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+      .setAppName(getClass.getSimpleName)
+
+    var (brokers, outputTopic, inputPath, numPartitions) = ("", "", "", "")
+    if (args.size == 4) {
+      brokers = args(0)
+      outputTopic = args(1)
+      inputPath = args(2)
+      numPartitions = args(3)
+    } else {
+      brokers = "localhost:9092"
+      outputTopic = "seeds"
+      inputPath = "/Users/dmitry/umbrella/seeds_sample"
+      numPartitions = "200"
+      conf.setMaster("local[*]")
+    }
+    val sc = new SparkContext(conf)
+    try {
+
+      val rawSeeds = sc.objectFile[(String)](inputPath, numPartitions.toInt)
+      val parsedSeeds = rawSeeds.map { line =>
+        try { MEnrichMessage.string2Message(line).toJson().toString().getBytes() }
+        catch {
+          case e: Exception => null
+        }
+      }.filter { _!=null }
+      //Producer: launch the Array[Byte]result into kafka      
+      Utils.pushByteRDD2Kafka(parsedSeeds, outputTopic, brokers)
+      println("!@!@!@!@!   rawSeeds Count:"+rawSeeds.count())
+      println("!@!@!@!@!   parsedSeeds Count:"+parsedSeeds.count())
+    } catch {
+      case e: Exception => {
+        println("########  Somthing went wrong :( ")
+        e.printStackTrace()
+      }
+    }
+  }
+}
+
+

1cffecd3dd40401d4477e95f3d559b3d35ef1c99
2dmitrypavlov <2dmitrypavlov@gmail.com>
2015-06-30 15:46:39 +0300

    Fixed filter on updatedPrice = 0 in Htmls2PredsPipe

--- src/main/java/um/re/streaming/Htmls2PredsPipe.scala
+++ src/main/java/um/re/streaming/Htmls2PredsPipe.scala
@@ -20,8 +20,8 @@ import org.apache.spark._
 object Htmls2PredsPipe extends App {
 
   val conf = new SparkConf()
-  	.setAppName(getClass.getSimpleName)
-  
+    .setAppName(getClass.getSimpleName)
+
   var inputMessagesCounter = 0L
   var parsedMessagesCounter = 0L
   var filterSuccesfulUpdatePriceMessagesCounter = 0L
@@ -32,6 +32,8 @@ object Htmls2PredsPipe extends App {
   var allFalseCandidsCounter = 0L
   var modeledEqualsUpdatedCounter = 0L
   var modeledNotEqualsUpdatedCounter = 0L
+
+  var exceptionCounter = 0L
 
   var (brokers, inputTopic, outputTopic, dMapPath, modelsPath) = ("", "", "", "", "")
   if (args.size == 5) {
@@ -52,115 +54,125 @@ object Htmls2PredsPipe extends App {
   val ssc = new StreamingContext(sc, Seconds(5))
 
   val dMap = sc.broadcast(sc.textFile((dMapPath), 1).collect().mkString("\n").split("\n").map(l => (l.split("\t")(0), l.split("\t")(1))).toMap)
+  try {
+    // Create direct kafka stream with brokers and topics
+    //TODO consider using multiple receivers for parallelism
+    val topicsSet = inputTopic.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+    val input = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder](
+      ssc, kafkaParams, topicsSet)
 
-  // Create direct kafka stream with brokers and topics
-  //TODO consider using multiple receivers for parallelism
-  val topicsSet = inputTopic.split(",").toSet
-  val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
-  val input = KafkaUtils.createDirectStream[String, Array[Byte], StringDecoder, DefaultDecoder](
-    ssc, kafkaParams, topicsSet)
-
-  val parsed = Utils.parseMEnrichMessage(input)
-  //TODO this below filter doesn't filter all zeros , need to fix
-  val filterSuccesfulUpdatePrice = parsed.filter{case(msg,msgMap) => 
-    (Utils.parseDouble(msgMap.apply("updatedPrice")).get-0.0).abs > 0.001}
-  
-  val candidates = filterSuccesfulUpdatePrice.transform(rdd => Utils.htmlsToCandidsPipe(rdd))
-
-  val predictions = candidates.map {
-    case (msg, candidList) =>
-      try{
-      val url = candidList.head.apply("url")
-      val domain = Utils.getDomain(url)
-      val domainCode = dMap.value.apply(domain)
-      val (model, idf, selected_indices) = sc.objectFile[(GradientBoostedTreesModel, Array[Double], Array[Int])](modelsPath + domainCode + "/part-00000", 1).first
-
-      val modelPredictions = candidList.map { candid =>
-        val priceCandidate = candid.apply("priceCandidate")
-        val location = Integer.valueOf(candid.apply("location")).toDouble
-        val text_before = Utils.tokenazer(candid.apply("text_before"))
-        val text_after = Utils.tokenazer(candid.apply("text_after"))
-        val text = text_before ++ text_after
-
-        val hashingTF = new HashingTF(1000)
-        val tf = Transformer.projectByIndices(hashingTF.transform(text).toArray, selected_indices)
-        val tfidf = (tf, idf).zipped.map((tf, idf) => tf * idf)
-
-        val features = tfidf ++ Array(location)
-        val values = features.filter { l => l != 0 }
-        val index = features.zipWithIndex.filter { l => l._1 != 0 }.map { l => l._2 }
-        val featureVec = Vectors.sparse(features.length, index, values)
-
-        val prediction = model.predict(featureVec)
-        val confidence = Transformer.confidenceGBT(model, featureVec)
-        (confidence, prediction, priceCandidate)
-      }
-      //TODO deal with extreme case e.g. all candidates are negative
-      var selectedCandid = (0.0, 0.0, "0")
-      if (modelPredictions.filter(c => c._2 == 1).size >= 1)
-        selectedCandid = modelPredictions.filter(c => c._2 == 1).sorted.reverse.head
-      else
-        selectedCandid = (0, 0, "-1")
-
-      val predictedPrice = selectedCandid._3
-      val msgObj: MEnrichMessage = MEnrichMessage.string2Message(msg)
-      msgObj.setModelPrice(predictedPrice)
-      msgObj.sethtml("")
-      msgObj
-      }
-      catch {
-        //TODO better log exceptions
-        case _: Exception => null
-      }
-  }.filter(_!=null)
-
-  predictions.map { msgObj =>
-    val modelPrice = Utils.parseDouble(msgObj.getModelPrice())
-    val updatedPrice = Utils.parseDouble(msgObj.getUpdatedPrice())
-    var status = ""
-
-    if (modelPrice.get == -1.0) {
-      status = "allFalseCandids"
-    } else {
-      if (updatedPrice != None && modelPrice != None && (modelPrice.get == updatedPrice.get) )
-        status = "modeledEqualsUpdated"
-      else
-        status = "modeledNotEqualsUpdated"
+    val parsed = Utils.parseMEnrichMessage(input)
+    //TODO this below filter doesn't filter all zeros , need to fix
+    val filterSuccesfulUpdatePrice = parsed.filter {
+      case (msg, msgMap) =>
+        println("xxxxxxxxxx " + Utils.parseDouble(msgMap.apply("updatedPrice")).get.toInt)
+        (Utils.parseDouble(msgMap.apply("updatedPrice")).get.toInt != 0)
     }
-    status
-  }.countByValue().foreachRDD { rdd =>
-    rdd.foreach {
-      case (counterType, count) =>
-        counterType match {
-          case "allFalseCandids" => allFalseCandidsCounter += count
-          case "modeledEqualsUpdated" => modeledEqualsUpdatedCounter += count
-          case "modeledNotEqualsUpdated" => modeledNotEqualsUpdatedCounter += count
+
+    val candidates = filterSuccesfulUpdatePrice.transform(rdd => Utils.htmlsToCandidsPipe(rdd))
+
+    val predictions = candidates.map {
+      case (msg, candidList) =>
+        try {
+          val url = candidList.head.apply("url")
+          val domain = Utils.getDomain(url)
+          val domainCode = dMap.value.apply(domain)
+          val (model, idf, selected_indices) = sc.objectFile[(GradientBoostedTreesModel, Array[Double], Array[Int])](modelsPath + domainCode + "/part-00000", 1).first
+
+          val modelPredictions = candidList.map { candid =>
+            val priceCandidate = candid.apply("priceCandidate")
+            val location = Integer.valueOf(candid.apply("location")).toDouble
+            val text_before = Utils.tokenazer(candid.apply("text_before"))
+            val text_after = Utils.tokenazer(candid.apply("text_after"))
+            val text = text_before ++ text_after
+
+            val hashingTF = new HashingTF(1000)
+            val tf = Transformer.projectByIndices(hashingTF.transform(text).toArray, selected_indices)
+            val tfidf = (tf, idf).zipped.map((tf, idf) => tf * idf)
+
+            val features = tfidf ++ Array(location)
+            val values = features.filter { l => l != 0 }
+            val index = features.zipWithIndex.filter { l => l._1 != 0 }.map { l => l._2 }
+            val featureVec = Vectors.sparse(features.length, index, values)
+
+            val prediction = model.predict(featureVec)
+            val confidence = Transformer.confidenceGBT(model, featureVec)
+            (confidence, prediction, priceCandidate)
+          }
+          //TODO deal with extreme case e.g. all candidates are negative
+          var selectedCandid = (0.0, 0.0, "0")
+          if (modelPredictions.filter(c => c._2 == 1).size >= 1)
+            selectedCandid = modelPredictions.filter(c => c._2 == 1).sorted.reverse.head
+          else
+            selectedCandid = (0, 0, "-1")
+
+          val predictedPrice = selectedCandid._3
+          val msgObj: MEnrichMessage = MEnrichMessage.string2Message(msg)
+          msgObj.setModelPrice(predictedPrice)
+          msgObj.sethtml("")
+          msgObj
+        } catch {
+          //TODO better log exceptions
+          case _: Exception => null
         }
+    }.filter(_ != null)
+
+    predictions.map { msgObj =>
+      val modelPrice = Utils.parseDouble(msgObj.getModelPrice())
+      val updatedPrice = Utils.parseDouble(msgObj.getUpdatedPrice())
+      var status = ""
+
+      if (modelPrice.get == -1.0) {
+        status = "allFalseCandids"
+      } else {
+        if (updatedPrice != None && modelPrice != None && (modelPrice.get == updatedPrice.get))
+          status = "modeledEqualsUpdated"
+        else
+          status = "modeledNotEqualsUpdated"
+      }
+      status
+    }.countByValue().foreachRDD { rdd =>
+      rdd.foreach {
+        case (counterType, count) =>
+          counterType match {
+            case "allFalseCandids"         => allFalseCandidsCounter += count
+            case "modeledEqualsUpdated"    => modeledEqualsUpdatedCounter += count
+            case "modeledNotEqualsUpdated" => modeledNotEqualsUpdatedCounter += count
+          }
+      }
+    }
+    val output = predictions.map(msgObj => msgObj.toJson().toString().getBytes())
+
+    input.count().foreachRDD(rdd => { inputMessagesCounter += rdd.first() })
+    parsed.count().foreachRDD(rdd => { parsedMessagesCounter += rdd.first() })
+    filterSuccesfulUpdatePrice.count().foreachRDD(rdd => { filterSuccesfulUpdatePriceMessagesCounter += rdd.first() })
+    candidates.count().foreachRDD(rdd => { candidatesMessagesCounter += rdd.first() })
+    predictions.count().foreachRDD(rdd => { predictionsMessagesCounter += rdd.first() })
+    output.count().foreachRDD { rdd =>
+      { outputMessagesCounter += rdd.first() }
+      println("!@!@!@!@!   inputMessagesCounter " + inputMessagesCounter)
+      println("!@!@!@!@!   parsedMessagesCounter " + parsedMessagesCounter)
+      println("!@!@!@!@!   filterSuccesfulUpdatePriceMessagesCounter " + filterSuccesfulUpdatePriceMessagesCounter)
+      println("!@!@!@!@!   candidatesMessagesCounter " + candidatesMessagesCounter)
+      println("!@!@!@!@!   predictionsMessagesCounter " + predictionsMessagesCounter)
+      println("!@!@!@!@!   outputMessagesCounter " + outputMessagesCounter)
+      println("!@!@!@!@!   allFalseCandidsCounter " + allFalseCandidsCounter)
+      println("!@!@!@!@!   modeledEqualsUpdatedCounter " + modeledEqualsUpdatedCounter)
+      println("!@!@!@!@!   modeledNotEqualsUpdatedCounter " + modeledNotEqualsUpdatedCounter)
+      println("!@!@!@!@!   exceptionCounter " + exceptionCounter)
+    }
+
+    output.foreachRDD { rdd =>
+      Utils.pushByteRDD2Kafka(rdd, outputTopic, brokers)
+    }
+  } catch {
+    case e: Exception => {
+      exceptionCounter += 1
+      println("########  Somthing went wrong :( ")
+      e.printStackTrace()
     }
   }
-  val output = predictions.map(msgObj => msgObj.toJson().toString().getBytes())
-
-  input.count().foreachRDD(rdd => { inputMessagesCounter += rdd.first() })
-  parsed.count().foreachRDD(rdd => { parsedMessagesCounter += rdd.first() })
-  filterSuccesfulUpdatePrice.count().foreachRDD(rdd => { filterSuccesfulUpdatePriceMessagesCounter += rdd.first() })
-  candidates.count().foreachRDD(rdd => { candidatesMessagesCounter += rdd.first() })
-  predictions.count().foreachRDD(rdd => { predictionsMessagesCounter += rdd.first() })
-  output.count().foreachRDD { rdd => { outputMessagesCounter += rdd.first() }
-    println("!@!@!@!@!   inputMessagesCounter " + inputMessagesCounter)
-    println("!@!@!@!@!   parsedMessagesCounter " + parsedMessagesCounter)
-    println("!@!@!@!@!   filterSuccesfulUpdatePriceMessagesCounter " + parsedMessagesCounter)
-    println("!@!@!@!@!   candidatesMessagesCounter " + candidatesMessagesCounter)
-    println("!@!@!@!@!   predictionsMessagesCounter " + predictionsMessagesCounter)
-    println("!@!@!@!@!   outputMessagesCounter " + outputMessagesCounter)
-    println("!@!@!@!@!   allFalseCandidsCounter " + allFalseCandidsCounter)
-    println("!@!@!@!@!   modeledEqualsUpdatedCounter " + modeledEqualsUpdatedCounter)
-    println("!@!@!@!@!   modeledNotEqualsUpdatedCounter " + modeledNotEqualsUpdatedCounter)
-  }
-
-  output.foreachRDD { rdd =>
-    Utils.pushByteRDD2Kafka(rdd, outputTopic, brokers)
-    }
-
   ssc.start()
   ssc.awaitTermination()
 

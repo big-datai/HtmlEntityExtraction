25a5fdad0bc5dfe85c85bfe964dfb70b32e2429c
Eran Sandler <eran.sandler1982@gmail.com>
2015-08-20 14:12:32 +0300

    bad domain analysis and Url Position and Variance

--- src/main/java/um/re/analysis/UrlPositionAndVar.scala
+++ src/main/java/um/re/analysis/UrlPositionAndVar.scala
@@ -1,3 +1,4 @@
+
 package um.re.analysis
 
 import org.apache.spark.SparkConf
@@ -9,26 +10,31 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import um.re.utils.Utils
+import org.apache.spark.HashPartitioner
+//import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.util.StatCounter
 
 object UrlPositionAndVar {
  def main(args:Array[String]) {
     val conf = new SparkConf()
       .setAppName(getClass.getSimpleName)
-     var (cassandraHost, keySpace, tableRT,tablePV, threshold ,path) = ("", "", "","","","")
+     var (cassandraHost, keySpace, tableRT,tableVPT, threshold ,path,numParts) = ("", "", "","","","","")
     if (args.size == 5) {
       cassandraHost = args(0)
       keySpace = args(1)
       tableRT = args(2)
-      tablePV = args(3)
+      tableVPT = args(3)
       threshold = args(4)
       path=args(5)
+      numParts=args(6)
     } else {
       cassandraHost = "localhost"
       val keySpace = "demo"
       val tableRT="real_time_market_prices"
-      val tablePT = "real_time_price_position"
+      val tableVPT = "real_time_price_pos_var"
       val threshold = "0.3"
       val path="/home/ec2-user"
+      val numParts = "28"
       conf.setMaster("local[*]")
     } 
    
@@ -43,6 +49,66 @@ object UrlPositionAndVar {
       }
     }
     
+    conf.set("spark.cassandra.connection.host", cassandraHost)
+    val sc = new SparkContext(conf)
+    val partitioner = new HashPartitioner(numParts.toInt)
+    
+    val RtData = sc.cassandraTable(keySpace, tableRT).map{row => 
+        val sys_prod_id = row.get[String]("sys_prod_id") 
+        val store_id = row.get[String]("store_id")
+        val price = row.get[Double]("price") 
+        val url = row.get[String]("url")
+        (sys_prod_id,(store_id,price,url))
+        }.partitionBy(partitioner)
+   
+  
+    
+    
+     
+  RtData.groupByKey().flatMap{
+                          case(sys_prod_id,iter) =>
+                         // val sze = iter.size
+                          var cnt=0            
+                          val NewTuple = iter.map{
+                                   case(store_id,price,url) =>(price,(store_id,url))}.toList.sorted.map{
+                                     case(price,(store_id,url)) => 
+                                         cnt+=1
+                                         (sys_prod_id,price,store_id,url,cnt)}
+                          val sze=NewTuple.size
+                          val priceList = NewTuple.map{case(sys_prod_id,price,store_id,url,cnt) => price}
+                          val variance = StatCounter(priceList).stdev 
+                          val FinalTuples=NewTuple.map{case(sys_prod_id,price,store_id,url,cnt) =>
+                            val relPlace=sze//(cnt/sze).toDouble
+                            (sys_prod_id,price,store_id,url,cnt,relPlace,variance)
+                       
+                          }
+                          FinalTuples
+                   // sc.parallelize(FinalTuples,1).saveToCassandra(keySpace, tableVPT, SomeColumns("sys_prod_id","price","domain","url","place","relPlace","variance"))    
+                }.saveToCassandra(keySpace, tableVPT, SomeColumns("sys_prod_id","price","domain","url","place","relplace","variance"))
+    
+    
+    
+    
+          /*
+    RtData.groupByKey().map{g =>
+         var cnt=0
+         val NewTuple =  g._2.map(l =>(l._2,(l._1,l._3))).toList.sorted.map{m => 
+         cnt+=1
+         (g._1,m._1,m._2._1,m._2._2,cnt)
+         }
+     
+         val variance = Statistics.colStats( NewTuple.)
+        }
+        */
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
     conf.set("spark.cassandra.connection.host", cassandraHost)
     val sc = new SparkContext(conf)  
     val cc = new CassandraSQLContext(sc)
@@ -82,9 +148,9 @@ object UrlPositionAndVar {
     val OrderedByPrice = realTime.filter(realTime("sys_prod_id")===l.toString()).count
   */    
       
+    */
     
     
-    
-    
-  }
+        }   
+  
 }

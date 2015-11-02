c706c219747bc7bea1764c81bd0544026046292a
2dmitrypavlov <2dmitrypavlov@gmail.com>
2015-11-02 10:53:00 +0200

    ..

--- src/main/java/um/re/emr/S3ToKafka.scala
+++ src/main/java/um/re/emr/S3ToKafka.scala
@@ -20,6 +20,7 @@ object S3ToKafka { //}extends App {
       .setAppName(getClass.getSimpleName)
 //./spark-shell --jars /home/ec2-user/core-1.0-SNAPSHOT-jar-with-dependencies.jar
     var (brokers, outputTopic, inputPath, numPartitions) = ("localhost:9092", "seeds1", "s3n://AKIAJQUAOI7EBC6Y7ESQ:JhremVoqNuEYG8YS9J+duW0hFRtX+sWjuZ0vdQlE@dpavlov/seedsReduce1_MonAug24154530UTC2015", "600")
+    //var (brokers, outputTopic, inputPath, numPartitions) = ("localhost:9092", "preseeds", "s3n://AKIAJQUAOI7EBC6Y7ESQ:JhremVoqNuEYG8YS9J+duW0hFRtX+sWjuZ0vdQlE@dpavlov/seedsReduce1_MonAug24154530UTC2015", "60")
     if (args.size == 4) {
       brokers = args(0)
       outputTopic = args(1)
@@ -53,9 +54,12 @@ object S3ToKafka { //}extends App {
 
      brokers= AWSUtils.getPrivateIp(brokers.substring(0, brokers.length() - 5)) + ":9092"
 
-      val rawSeeds = sc.textFile(inputPath, numPartitions.toInt) //sc.objectFile[(String)](inputPath, numPartitions.toInt).cache
+      val rawSeeds = sc.textFile(inputPath, numPartitions.toInt)//.sample(true, 0.1, 123) //sc.objectFile[(String)](inputPath, numPartitions.toInt).cache
       val parsedSeeds = rawSeeds.map { line =>
-        try { BigMessage.string2Message(line).toJson().toString().getBytes() }
+        try { 
+          val res=BigMessage.string2Message(line).toJson().toString()
+          print(res)
+          res.getBytes() }
         catch {
           case e: Exception => null
         }
